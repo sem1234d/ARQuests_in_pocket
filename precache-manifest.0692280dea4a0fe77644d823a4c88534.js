4e705670b6629146be0f81b422aa3b7f917a7385
sem1234d <sem1234d@gmail.com>
2019-11-17 21:56:19 +0300

    Updates

---
+++ precache-manifest.0692280dea4a0fe77644d823a4c88534.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca45df4da9a178390474d52befdf8e2",
+    "url": "/ARQuests_in_pocket/index.html"
+  },
+  {
+    "revision": "44b5586878ef4049ba58",
+    "url": "/ARQuests_in_pocket/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "918f3c0bdcd9d87c899b",
+    "url": "/ARQuests_in_pocket/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "44b5586878ef4049ba58",
+    "url": "/ARQuests_in_pocket/static/js/2.04402b13.chunk.js"
+  },
+  {
+    "revision": "918f3c0bdcd9d87c899b",
+    "url": "/ARQuests_in_pocket/static/js/main.b388bfa5.chunk.js"
+  },
+  {
+    "revision": "2a95411f975160613962",
+    "url": "/ARQuests_in_pocket/static/js/runtime-main.0b4f3700.js"
+  },
+  {
+    "revision": "88fc8da66a348c818f93ff9cfce70d64",
+    "url": "/ARQuests_in_pocket/static/media/cof.88fc8da6.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ARQuests_in_pocket/static/media/logo.25bf045c.svg"
+  }
+]);
