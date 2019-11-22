9bb0541cb037a8b5d9b1a28f0c68cc2b75dff30d
sem1234d <sem1234d@gmail.com>
2019-11-22 13:33:42 +0300

    Updates

--- precache-manifest.6e357af629830b7e138928581036042a.js
+++ precache-manifest.6e357af629830b7e138928581036042a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b2f81ec5449aee1d113fc2f3f7f8457",
+    "revision": "9ebc0e9da33d42195c5636329f9bd7e8",
     "url": "/ARQuests_in_pocket/index.html"
   },
   {
-    "revision": "fa4c5a9c3d2ab294259d",
+    "revision": "e0fac0065bd40124c263",
     "url": "/ARQuests_in_pocket/static/css/2.266626f9.chunk.css"
   },
   {
-    "revision": "41fe56e3f98337e80dca",
+    "revision": "0d2ca92e80d7899523f1",
     "url": "/ARQuests_in_pocket/static/css/main.1a8d6906.chunk.css"
   },
   {
-    "revision": "fa4c5a9c3d2ab294259d",
-    "url": "/ARQuests_in_pocket/static/js/2.2e64dc0b.chunk.js"
+    "revision": "e0fac0065bd40124c263",
+    "url": "/ARQuests_in_pocket/static/js/2.9c00deec.chunk.js"
   },
   {
-    "revision": "41fe56e3f98337e80dca",
-    "url": "/ARQuests_in_pocket/static/js/main.9a5884cd.chunk.js"
+    "revision": "0d2ca92e80d7899523f1",
+    "url": "/ARQuests_in_pocket/static/js/main.000e0ed3.chunk.js"
   },
   {
     "revision": "2a95411f975160613962",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ARQuests_in_pocket/precache-manifest.2da4b0c5f84278f5d4265d2bb098c4a7.js"
+  "/ARQuests_in_pocket/precache-manifest.6e357af629830b7e138928581036042a.js"
 );
 
 self.addEventListener('message', (event) => {
